83f612706a08d675fa58e974e78b9439938f6aa3
Victor Grebennikov <victorgrebennikov4@gmail.com>
2024-01-31 21:41:33 +0700

    Some change in base.html, blueprint.py

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,4 +1,11 @@
 from flask import Blueprint
+from flask import render_template  # импортируем функцию render_template
 
 # В переменную posts помещаем конструктор класса Blueprint ->
 posts = Blueprint('posts', __name__, template_folder='templates')
+
+
+@posts.route('/')  # Создаём вьюху
+def index():
+    return render_template(
+        'posts/index.html')  # имя шаблона из posts\template\posts
